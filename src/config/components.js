c82c7c2485d6f03507c4f28e60e42d624bb2d470
jbourlard <jason.bourlard@soprasteria.com>
2020-02-25 19:35:37 +0100

    add RadioReceiver.js

--- src/config/components.js
+++ src/config/components.js
@@ -1,6 +1,7 @@
 import Button from "../components/Button";
 import Switch from "../components/Switch";
 import SwitchUpDown from "../components/SwitchUpDown";
+import RadioReceiver from "../components/RadioReceiver";
 
 const components = {
   // Switch
@@ -15,7 +16,9 @@ const components = {
 
     // Stop Button
     buttonStop : new Button(22),
-  })
+  }),
+
+  radioReceiver : new RadioReceiver(12),
 }
 
 export default components;
